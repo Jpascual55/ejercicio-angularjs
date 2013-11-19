e9af9a1998b75b55952c529f39c59fa93052c3d8
Javier Pascual <javier.pascual.adesis@gmail.com>
2013-11-19 10:57:34 +0100

    merge

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,22 +3,25 @@
 var motogpControllers = angular.module('motogpControllers', []);
 
 motogpControllers.controller('pilotsListCtrl',
- function ($scope, $http) {
+ function($scope, $http) {
   $http.get('datos/pilotos.json').success(function(data) {
     $scope.pilotos = data;
-   });
-});
+  });
+}
+);
 
 motogpControllers.controller('pilotCtrl',
   function($scope, $routeParams, $http) {
     $http.get('datos/' + $routeParams.pilotoId + '.json').success(function(data) {
       $scope.piloto = data;
     });
-});
+  }
+);
 
 motogpControllers.controller('teamCtrl',
   function($scope, $routeParams, $http) {
     $http.get('datos/' + $routeParams.equipoId + '.json').success(function(data) {
       $scope.equipo = data;
     });
-});
+  }
+);
